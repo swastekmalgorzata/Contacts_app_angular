c5ac8d74ad81871810b0c1b2aee922a9a4813f71
Malgorzata Swastek <swastekm@gmail.com>
2023-07-02 11:39:16 +0200

    typos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { HttpHeaders+ } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
